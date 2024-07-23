948df9158888cab83607104207271bfd7d2b05c7
Sierra <sierradmichel@gmail.com>
2024-07-23 19:00:02 -0400

    Initial commit

---
+++ Arithmetic Operations.py
@@ -0,0 +1,4 @@
+# Task 1: Grocery Store Math Calculate the total cost of three items you'd commonly find in a grocery store, given their individual prices. For example, what would be the cost of bread, peanut butter, and jelly be? Prices don't need to be realistic!
+
+
+# Task 2: Bank Interest If you have a savings account with a particular initial amount and a fixed yearly interest rate, calculate the total amount in your account after a year. So if you had $10,000 at a 7% interest write code that would tell us the amount at the end of the year. For the example the expected outcome would be $10,700.
